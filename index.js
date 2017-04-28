051f7ca30390a8283a67d2e898506136dd17ad80
szrharrison <s.z.r.harrison@gmail.com>
2017-04-28 15:24:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+
+var dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left < 400) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(key) {
+  if (key.which === 37) {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(key) {
+  if (key.which === 39) {
+    moveDodgerRight()
+  }
+})
+
+document.addEventListener('keydown', function(key) {
+  console.log(key.which)
+})
